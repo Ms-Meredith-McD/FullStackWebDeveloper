842204078b2cd0044fee3f20e12bc2c418807356
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-10 14:41:14 -0500

    trying to fix mobile view

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -23,38 +23,38 @@ export default function Project() {
         <div className="mb-4 pt-5">
             <div className="row row-cols-1 row-cols-md-2 g-4 pt-5">
                 {projectData.map((project) => (
-                    <div className="col">
-                        <div className="card col h-100">
-                            <img src={`./${project.gitHubName}.png`} className="card-img-top" alt="" />
-                            <img src={project.image} alt="Project Screenshot" />
-                            <div className="card-body" id={project.id} key={project.title}>
-                                <h5 className="card-title">{project.title}</h5>
-                                <div className="card-body">
-                                    <a
-                                        href={generateDeployedURL(
-                                            project.deploymentMethod,
-                                            project.deployedName,
-                                            project?.gitHubUser
-                                        )}
-                                        className="card-link"
-                                        target="_blank"
-                                    >
-                                        Deployed App
-                                    </a>
-                                    <a
-                                        href={generateRepositoryURL(
-                                            project.gitHubName,
-                                            project.gitHubUser
-                                        )}
-                                        className="card-link"
-                                        target="_blank"
-                                    >
-                                        GitHub Repository
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div className="col" key={project.title}>
+    <div className="card h-100">
+        {/* Use the project.image directly */}
+        <img src={project.image} className="card-img-top" alt="Project Screenshot" />
+        <div className="card-body" id={project.id}>
+            <h5 className="card-title">{project.title}</h5>
+            <div className="card-body">
+                <a
+                    href={generateDeployedURL(
+                        project.deploymentMethod,
+                        project.deployedName,
+                        project?.gitHubUser
+                    )}
+                    className="card-link"
+                    target="_blank"
+                >
+                    Deployed App
+                </a>
+                <a
+                    href={generateRepositoryURL(
+                        project.gitHubName,
+                        project.gitHubUser
+                    )}
+                    className="card-link"
+                    target="_blank"
+                >
+                    GitHub Repository
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
                 ))}
             </div>
             </div>
