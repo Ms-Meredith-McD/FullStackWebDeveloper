50c52e61f1947202b935513de4d8281709e90d4b
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-09 21:25:59 -0600

    fixed footer

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
     return (
         <section className="footer">
             <div>
-                <h2 id="contact-me">Contact Me</h2>
                 <ol>
+                <h4 id="contact-me">Contact Me:</h4>
                     <li><a href="tel:612-701-7268">612-701-7268</a></li>
                     <li><a href="mailto:meredith.e.mcdonald@gmail.com">meredith.e.mcdonald@gmail.com</a></li>
                     <li><a href="https://github.com/Ms-Meredith-McD?tab=repositories"><i className="fab fa-github"></i>Github</a></li>
