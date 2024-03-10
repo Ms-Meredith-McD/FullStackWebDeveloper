6d28528960b2c81a9925587b43dfe0749f509bb6
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-09 21:04:52 -0600

    remove instagram

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -8,7 +8,6 @@ function Footer() {
                     <li><a href="mailto:meredith.e.mcdonald@gmail.com">meredith.e.mcdonald@gmail.com</a></li>
                     <li><a href="https://github.com/Ms-Meredith-McD?tab=repositories"><i className="fab fa-github"></i>Github</a></li>
                     <li><a href="https://www.linkedin.com/in/memcdonald/"><i className="fab fa-linkedin"></i>LinkedIn</a></li>
-                    <li><a href="https://www.instagram.com/ms_meredith_mcd/"><i className="fab fa-instagram"></i>Instagram</a></li>
                 </ol>
             </div>
         </section>
