c1f133e97b0dbe1ac772070673ad2acc83cdec9a
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-15 14:06:08 -0500

    adding more info to my aboutme

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,7 +8,9 @@ export default function About() {
         <h1>About Me</h1>
         <div>
           <img src={headshotImage} alt="Meredith" />
-          <h1>Full Stack Web Developer</h1>
+          <h2 className="text-center">Full Stack Web Developer</h2>
+          <p style={{ textAlign: "left" }}>After spending several years as a financial advisor and then in HR, I was ready for something new. I'm looking for a career that is more in tune with my love of logic, technology, and solving problems. I've recently come up with a phrase that I've been using to sum up my goal:</p>
+          <h4>"Show me what you want & I will build my way to it!"</h4>
         </div>
       </div>
     </div>
